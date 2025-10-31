90a7bba4125813ad2bc840383a102a3533c3cb40
Terryxz1 <T.YAVOR@GMAIL.COM>
2025-10-31 13:15:10 -0400

    Added Uterus to th e metabolism. Luckily the heart throws an error before the uterus!

---
+++ restart-hummod.ps1
@@ -0,0 +1,11 @@
+# restart-hummod.ps1
+$humModPath = "C:\Users\T347A55\Documents\GitHub\HumMod-UTSC-CPG\HumMod.EXE"
+
+# Kill existing HumMod instance (if running)
+Get-Process "HumMod" -ErrorAction SilentlyContinue | Stop-Process
+
+# Small delay to ensure it shuts down
+Start-Sleep -Milliseconds 500
+
+# Start HumMod again
+Start-Process $humModPath
